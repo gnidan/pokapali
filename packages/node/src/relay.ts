01581655a2a1880b6635f0e6f27abfaa158c8c22
g. nicholas d'andrea <nick@gnidan.org>
2026-03-10 05:11:02 -0400

    debug: log caps broadcast neighbor/browser/peer counts

--- packages/node/src/relay.ts
+++ packages/node/src/relay.ts
@@ -492,6 +492,11 @@ export async function startRelay(
       }
     }
 
+    log.info(
+      `caps: ${neighbors.length} neighbors,`
+      + ` ${browserCount} browsers,`
+      + ` ${knownPeerRoles.size} known peers`,
+    );
     const msg = encodeNodeCaps({
       version: 2,
       peerId: selfPeerId,
