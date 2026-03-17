53eca83465cd88d08cd6a42c74c2a3bfab05dd3e
pokapali-testing <pokapali-testing@pokapali.local>
2026-03-17 02:44:07 -0400

    fix(test-utils): cast Promise to any to avoid TS2550
    The tsconfig lib doesn't include es2024, so
    Promise.withResolvers isn't in the type definitions.
    Cast to any to avoid the build error.
    
    Co-authored-by: g. nicholas d'andrea <nick@gnidan.org>

--- packages/test-utils/src/setup.ts
+++ packages/test-utils/src/setup.ts
@@ -6,9 +6,10 @@
  * Safe to remove once minimum Node is 22+.
  */
 
-if (typeof Promise.withResolvers !== "function") {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (Promise as any).withResolvers = function <T>() {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const P = Promise as any;
+if (typeof P.withResolvers !== "function") {
+  P.withResolvers = function <T>() {
     let resolve!: (value: T | PromiseLike<T>) => void;
     let reject!: (reason?: unknown) => void;
     const promise = new Promise<T>((res, rej) => {
